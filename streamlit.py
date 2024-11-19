af9ff680315cbb028ff28f4dab97c93a9501d630
duyenltm <duyenltm2004@gmail.com>
2024-11-19 22:22:30 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,10 @@
+pip install streamlit pandas scikit-learn
 import streamlit as st
 import pandas as pd
-#from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import MinMaxScaler
-#from sklearn.preprocessing import LabelEncoder
-#from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
 
 st.title("""
 Pakistan House Price Prediction App
