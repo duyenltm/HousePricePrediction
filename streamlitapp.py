e6f411f2c76740a0528040084d89ef11c419832d
duyenltm <duyenltm2004@gmail.com>
2024-11-26 17:29:25 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -42,8 +42,8 @@ data['location'] = le.fit_transform(data['location'])
 scaler = MinMaxScaler()
 data = scaler.fit_transform(data)
 
-X = data.drop(['price_scaled'], axis=1)
-y = data['price_scaled']
+X = data.drop(['price'], axis=1)
+y = data['price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
